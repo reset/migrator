342c6e20c9922faa7867ee43d425e6ddd140040a
Jamie Winsor <jamie@vialstudios.com>
2014-10-01 18:41:04 -0700

    version bump 0.2.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Migrator.Mixfile do
   def project do
     [
       app: :migrator,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.0.0 or ~> 0.15.1",
       deps: deps,
       escript: [
