276ed8285c3565e26fef91fa8e6e673c12475da7
Jamie Winsor <jamie@vialstudios.com>
2015-01-30 14:58:09 -0800

    add support for Ecto 0.7.x

--- lib/migrator/repo.ex
+++ lib/migrator/repo.ex
@@ -1,18 +1,13 @@
 defmodule Migrator.Repo do
   use Ecto.Repo, adapter: Ecto.Adapters.Postgres, otp_app: :migrator
-  import Migrator, only: [configuration: 0]
-
-  def conf do
-    Dict.to_list(configuration[:connection]) ++ [size: "1"]
-  end
 
   def create_schema(name) do
-    adapter.query(__MODULE__, "CREATE SCHEMA IF NOT EXISTS #{name}", [])
+    Ecto.Adapters.SQL.query(__MODULE__, "CREATE SCHEMA IF NOT EXISTS #{name}", [])
   end
 
   def set_schema(nil), do: :ok
   def set_schema(name) do
     create_schema(name)
-    adapter.query(__MODULE__, "SET search_path TO #{name}", [])
+    Ecto.Adapters.SQL.query(__MODULE__, "SET search_path TO #{name}", [])
   end
 end
