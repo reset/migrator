7da6388bddd61d0250bb91a518d11cec85bc2e3d
Jamie Winsor <jamie@vialstudios.com>
2015-02-12 16:49:19 -0800

    loosen constraint on Elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Migrator.Mixfile do
     [
       app: :migrator,
       version: "0.4.0",
-      elixir: "~> 1.0.0 or ~> 0.15.1",
+      elixir: "~> 1.0 or ~> 0.15.1",
       deps: deps,
       escript: [
         main_module: Migrator.CLI,
